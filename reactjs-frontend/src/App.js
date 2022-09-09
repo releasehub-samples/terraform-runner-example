d48ef12b98c7ba3b6b5b9b6da9862b229fb4c6fb
Mat Werber <28939541+matwerber1@users.noreply.github.com>
2022-09-08 22:13:22 -0700

    update

--- reactjs-frontend/src/App.js
+++ reactjs-frontend/src/App.js
@@ -1,19 +1,55 @@
 //import logo from './logo.svg';
 import './App.css';
 
-//import React, { useEffect, useState } from "react";
-import React from "react";
+import React, { useEffect, useState } from "react";
+//import React from "react";
 // import the react-json-view component
 //import JSONViewer from 'react-json-viewer';
 
-//import axios from "axios";
+import axios from "axios";
 
-const REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "Failed to retrieve REACT_APP_API_BASE_URL at build time.";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "Failed to retrieve REACT_APP_API_BASE_URL at build time.";
+
+const client = axios.create({
+	baseURL: process.env.REACT_APP_BACKEND_URL
+});
 
 function App() {
 
-  console.log('API_BASE_URL: ' + REACT_APP_API_BASE_URL.toString());
+  console.log('API_BASE_URL: ' + API_BASE_URL.toString());
   
+  const [data, setData] = useState({});
+  const [error, setError] = useState("");
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    getApiResponse();
+  }, []);
+
+  async function getApiResponse() {
+    ``;
+    try {
+      setLoaded(true);
+      const data = await getHelloMessage();
+      setData(data);
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoaded(false);
+    }
+  }
+
+  async function getHelloMessage() {
+    return await client.get(`${API_BASE_URL}/hello/hello`);
+  }
+
+  function ApiResults() {
+    if (error) {
+      return `Failed to retrieve API GW Response: ${JSON.stringify(error,null,2)}`;
+    } 
+    return loaded ? data.message : "Loading...";
+  }
+
   return (
     <div className="App">
       <div>
@@ -23,7 +59,8 @@ function App() {
       </div>
       <br/>
       <p>
-      ${REACT_APP_API_BASE_URL.toString()}
+        <b>API Gateway Results:</b><br/>
+          <ApiResults/>
       </p>
     </div>
   );
