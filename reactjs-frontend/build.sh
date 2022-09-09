4ee5862af06845d447b6a35595587c99fd499ec2
Mat Werber <28939541+matwerber1@users.noreply.github.com>
2022-09-08 18:21:50 -0700

    update

--- reactjs-frontend/build.sh
+++ reactjs-frontend/build.sh
@@ -11,10 +11,12 @@ PARAMETER_NAME="/releasehub/$RELEASE_APP_NAME/$RELEASE_BRANCH_NAME/$RELEASE_ENV_
 echo Attempting to retrieve parameter: $PARAMETER_NAME
 
 
-if ! RESULT=$(aws ssm get-parameter --name $PARAMETER_NAME  --region=$PARAMETER_REGION --query "Parameter.Value" --output text); then
-    echo "Error: failed to call aws ssm get-parameter --name $PARAMETER_NAME"    
+if ! REACT_APP_API_BASE_URL=$(aws ssm get-parameter --name $PARAMETER_NAME  --region=$PARAMETER_REGION --query "Parameter.Value" --output text); then
+    REACT_APP_API_BASE_URL="Error: failed to call aws ssm get-parameter --name $PARAMETER_NAME"  
 else
-    echo Retrieved API Base URL: $RESULT
-    export REACT_APP_API_BASE_URL=$RESULT
-    npm run build
-fi    
+    echo Retrieved API Base URL: $REACT_APP_API_BASE_URL
+fi
+
+echo API_BASE_URL: ${REACT_APP_API_BASE_URL}
+
+REACT_APP_API_BASE_URL="$REACT_APP_API_BASE_URL" npm run build
