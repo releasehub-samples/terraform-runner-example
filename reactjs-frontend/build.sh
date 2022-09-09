47e2ce412b7fe232664bc247b7d56cc578835327
Mat Werber <28939541+matwerber1@users.noreply.github.com>
2022-09-08 17:26:51 -0700

    update

--- reactjs-frontend/build.sh
+++ reactjs-frontend/build.sh
@@ -2,6 +2,7 @@
 
 # Retrieve the DNS endpoint of API Gateway previously created from our Terraform:
 PARAMETER_NAME="/releasehub/$RELEASE_APP_NAME/$RELEASE_BRANCH_NAME/$RELEASE_ENV_ID/api_base_url"
+echo Attempting to retrieve parameter: $PARAMETER_NAME
 export REACT_APP_API_BASE_URL=$(aws ssm get-parameter --name $PARAMETER_NAME)
 
 # Was retrieval successful?
