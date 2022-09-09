518ac72d1b3823da18b5699be581880cb3f502f1
Mat Werber <28939541+matwerber1@users.noreply.github.com>
2022-09-08 18:03:01 -0700

    fix typo

--- reactjs-frontend/build.sh
+++ reactjs-frontend/build.sh
@@ -7,11 +7,11 @@ aws sts get-caller-identity
 export PARAMETER_REGION=$RELEASE_CLUSTER_REGION
 
 # Retrieve the DNS endpoint of API Gateway previously created from our Terraform:
-PARAMETER_NAME="/releasehub/$RELEASE_APP_NAME/$RELEASE_BRANCH_NAME/$RELEASE_ENV_ID/api_base_url" --region=$PARAMETER_REGION
+PARAMETER_NAME="/releasehub/$RELEASE_APP_NAME/$RELEASE_BRANCH_NAME/$RELEASE_ENV_ID/api_base_url"
 echo Attempting to retrieve parameter: $PARAMETER_NAME
 
 
-if ! REACT_APP_API_BASE_URL=$(aws ssm get-parameter --name $PARAMETER_NAME); then
+if ! REACT_APP_API_BASE_URL=$(aws ssm get-parameter --name $PARAMETER_NAME  --region=$PARAMETER_REGION); then
     echo "Error: failed to call aws ssm get-parameter --name $PARAMETER_NAME"    
 else
     echo Retrieved API Base URL: $REACT_APP_API_BASE_URL
