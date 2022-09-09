d31082049b46ab0f43bf0f0668396cb5eb8b19d1
Mat Werber <28939541+matwerber1@users.noreply.github.com>
2022-09-08 17:53:38 -0700

    update

--- reactjs-frontend/build.sh
+++ reactjs-frontend/build.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-# Retrieve the DNS endpoint of API Gateway previously created from our Terraform:
-PARAMETER_NAME="/releasehub/$RELEASE_APP_NAME/$RELEASE_BRANCH_NAME/$RELEASE_ENV_ID/api_base_url"
-echo Attempting to retrieve parameter: $PARAMETER_NAME
-export REACT_APP_API_BASE_URL=$(aws ssm get-parameter --name $PARAMETER_NAME)
-
 echo "AWS IDENTITY":
 aws sts get-caller-identity
 
-# Was retrieval successful?
-if [ ! -z "$REACT_APP_API_BASE_URL" ]   
-then    
-    export REACT_APP_API_BASE_URL="Error: failed to call aws ssm get-parameter --name $PARAMETER_NAME"    
-    echo $REACT_APP_API_BASE_URL
+# Retrieve the DNS endpoint of API Gateway previously created from our Terraform:
+PARAMETER_NAME="/releasehub/$RELEASE_APP_NAME/$RELEASE_BRANCH_NAME/$RELEASE_ENV_ID/api_base_url"
+echo Attempting to retrieve parameter: $PARAMETER_NAME
+
+
+if ! REACT_APP_API_BASE_URL=$(aws ssm get-parameter --name $PARAMETER_NAME); then
+    echo "Error: failed to call aws ssm get-parameter --name $PARAMETER_NAME"    
 else
     echo Retrieved API Base URL: $REACT_APP_API_BASE_URL
+    npm run build
 fi    
-
-# Build react app: 
-npm run build
